67a65d86e2f1afb2d5d36f60411bf57c31728e3e
Nikolay Varfolomeev <varnikgen@yandex.ru>
2020-07-15 16:35:08 +1000

    добавлен django-filter для фильтрации по годам и жанрам

--- django_movie/movies/service.py
+++ django_movie/movies/service.py
@@ -1,8 +1,25 @@
+from django_filters import rest_framework as filters
+from .models import Movie
+
+
 def get_client_ip(request):
     """Получение IP пользователя"""
     x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
     if x_forwarded_for:
-        x_forwarded_for.split(',')[0]
+        ip = x_forwarded_for.split(',')[0]
     else:
         ip = request.META.get('REMOTE_ADDR')
     return ip
+
+
+class CharFilterInFilter(filters.BaseInFilter, filters.CharFilter):
+    pass
+
+
+class MovieFilter(filters.FilterSet):
+    genre = CharFilterInFilter(field_name='genre__name', lookup_expr='in')
+    year = filters.RangeFilter()
+
+    class Meta:
+        model = Movie
+        fields = ['genre', 'year']
